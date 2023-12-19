9987653f6f1d4750c2c1cdefa948ba4d3bfe6670
syny-pulse <sinaijose002@gmail.com>
2023-12-20 01:40:23 +0300

    Day 9, looking deeper into conditional statements

---
+++ day9.c
@@ -0,0 +1,13 @@
+year = int(input("Hey, what year were you born? "))
+if year >= 1996 :
+  print("Yoo, you are a Generation Z! \U0001F606")
+elif year >= 1982 :
+  print("Hey, Millenial! \U0001F60A")
+elif year >= 1965 :
+  print("You are a Generation X \U0001F60E!")
+elif year >= 1947 :
+  print("Hey Baby Boomer! \U0001F60C")
+elif year >= 1925 :
+  print("You are a Traditionalist \U0001F60D!")
+else:
+  print("Enter valid age!")
